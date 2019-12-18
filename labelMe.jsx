6ee136415092deafb400b51b0f66598ed7cfa75f
Brandon <brandy@scarecrowarts.com>
2019-12-18 18:46:06 -0500

    needs proper paqdding

--- labelMe.jsx
+++ labelMe.jsx
@@ -1,132 +1,190 @@
 ﻿//  Copyright (c) 2020 Scarecrow Arts
 //  http://www.scarecrowarts.com
+//  Version 0.01
+
+//NOT WORTH $19 USD THAT'S FOR SURE
 
 var scriptPath = File($.fileName).parent.fsName;
-
 var myPanel;
 var label; 
 
-
-
 function myScript(thisObj){
     function myScript_buildUI(thisObj){
-        myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "Easy Bake", undefined, {resizeable:false});
+        myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "labelME", [100, 100, 12, 560], {resizeable:false});
+        
+        var img01 = scriptPath + "/labelME/red.png"
+        var img02 = scriptPath + "/labelME/yellow.png"
+        var img03 = scriptPath + "/labelME/aqua.png"
+        var img04 = scriptPath + "/labelME/pink.png"
+        var img05 = scriptPath + "/labelME/lavender.png"
+        var img06 = scriptPath + "/labelME/peach.png"
+        var img07 = scriptPath + "/labelME/seafoam.png"
+        var img08 = scriptPath + "/labelME/blue.png"
+        var img09 = scriptPath + "/labelME/green.png"
+        var img10 = scriptPath + "/labelME/purple.png"
+        var img11 = scriptPath + "/labelME/orange.png"
+        var img12 = scriptPath + "/labelME/brown.png"
+        var img13 = scriptPath + "/labelME/fuchsia.png"
+        var img14 = scriptPath + "/labelME/cyan.png"
+        var img15 = scriptPath + "/labelME/sandstone.png"
+        var img16 = scriptPath + "/labelME/darkgreen.png"
 
-        res = "group\
-            {\
-            orientation:'column',\
-            myButton1: Button{text:'Red'},\
-            myButton2: Button{text:'Yellow'},\
-            myButton3: Button{text:'Auqa'},\
-            myButton4: Button{text:'Pink'},\
-            myButton5: Button{text:'Lavender'},\
-            myButton6: Button{text:'Peach'},\
-            myButton7: Button{text:'Sea Foam'},\
-            myButton8: Button{text:'Blue'},\
-            myButton9: Button{text:'Green'},\
-            myButton10: Button{text:'Purple'},\
-            myButton11: Button{text:'Orange'},\
-            myButton12: Button{text:'Brown'},\
-            myButton13: Button{text:'Fuchsia'},\
-            myButton14: Button{text:'Cyan'},\
-            myButton15: Button{text:'Sandstone'},\
-            myButton16: Button{text:'Dark Green'},\
-            }";
+        res = "group\{\orientation:'column',\}";
 
         myPanel.grp = myPanel.add(res);
+        
+        
+        myPanel.margins = 0;
+        myPanel.grp.margins = 0;
+        myPanel.grp.margins.top = 0;
+        myPanel.grp.margins.bottom = 0;
+        myPanel.grp.margins.left = 0;
+        myPanel.grp.margins.right = 0;
+        myPanel.grp.spacing = 0;
+        myPanel.grp.spacing.top = 0;
+        myPanel.grp.spacing.bottom = 0;
+        myPanel.grp.spacing.left = 0;
+        myPanel.grp.spacing.right = 0;
+        
+        //INITIALIZE BUTTONS
+        var button01 = myPanel.grp.add("iconbutton", undefined, img01, "button01")
+        var button02 = myPanel.grp.add("iconbutton", undefined, img02, "button02")
+        var button03 = myPanel.grp.add("iconbutton", undefined, img03, "button03")
+        var button04 = myPanel.grp.add("iconbutton", undefined, img04, "button04")
+        var button05 = myPanel.grp.add("iconbutton", undefined, img05, "button05")
+        var button06 = myPanel.grp.add("iconbutton", undefined, img06, "button06")
+        var button07 = myPanel.grp.add("iconbutton", undefined, img07, "button07")
+        var button08 = myPanel.grp.add("iconbutton", undefined, img08, "button08")
+        var button09 = myPanel.grp.add("iconbutton", undefined, img09, "button09")
+        var button10 = myPanel.grp.add("iconbutton", undefined, img10, "button10")
+        var button11 = myPanel.grp.add("iconbutton", undefined, img11, "button11")
+        var button12 = myPanel.grp.add("iconbutton", undefined, img12, "button12")
+        var button13 = myPanel.grp.add("iconbutton", undefined, img13, "button13")
+        var button14 = myPanel.grp.add("iconbutton", undefined, img14, "button14")
+        var button15 = myPanel.grp.add("iconbutton", undefined, img15, "button15")
+        var button16 = myPanel.grp.add("iconbutton", undefined, img16, "button16")
+        
+        //TOOL TIPS
+        button01.helpTip = "Red";
+        button02.helpTip = "Yellow";
+        button03.helpTip = "Aqua";
+        button04.helpTip = "Pink";
+        button05.helpTip = "Lavender";
+        button06.helpTip = "Peach";
+        button07.helpTip = "Sea Foam";
+        button08.helpTip = "Blue";
+        button09.helpTip = "Green";
+        button10.helpTip = "Purple";
+        button11.helpTip = "Orange";
+        button12.helpTip = "Brown";
+        button13.helpTip = "Fuchsia";
+        button14.helpTip = "Cyan";
+        button15.helpTip = "Sandstone";
+        button16.helpTip = "Dark Green";
 
+        //DO THE THING
+        button01.onClick =  onTabClicked01;
+        button02.onClick =  onTabClicked02;
+        button03.onClick =  onTabClicked03;       
+        button04.onClick =  onTabClicked04;      
+        button05.onClick =  onTabClicked05;
+        button06.onClick =  onTabClicked06;
+        button07.onClick =  onTabClicked07;
+        button08.onClick =  onTabClicked08;       
+        button09.onClick =  onTabClicked09;      
+        button10.onClick =  onTabClicked10;
+        button11.onClick =  onTabClicked11;
+        button12.onClick =  onTabClicked12;
+        button13.onClick =  onTabClicked13;       
+        button14.onClick =  onTabClicked14;
+        button15.onClick =  onTabClicked15;
+        button16.onClick =  onTabClicked16;
 
-        myPanel.grp.myButton1.onClick =  onTabClicked;
-        myPanel.grp.myButton2.onClick =  onTabClicked2;
-        myPanel.grp.myButton3.onClick =  onTabClicked3;       
-        myPanel.grp.myButton4.onClick =  onTabClicked4;      
-        myPanel.grp.myButton5.onClick =  onTabClicked5;
-        myPanel.grp.myButton6.onClick =  onTabClicked6;
-        myPanel.grp.myButton7.onClick =  onTabClicked7;
-        myPanel.grp.myButton8.onClick =  onTabClicked8;       
-        myPanel.grp.myButton9.onClick =  onTabClicked9;      
-        myPanel.grp.myButton10.onClick =  onTabClicked10;
-        myPanel.grp.myButton11.onClick =  onTabClicked11;
-        myPanel.grp.myButton12.onClick =  onTabClicked12;
-        myPanel.grp.myButton13.onClick =  onTabClicked13;       
-        myPanel.grp.myButton14.onClick =  onTabClicked14;
-        myPanel.grp.myButton15.onClick =  onTabClicked15;
-        myPanel.grp.myButton16.onClick =  onTabClicked16;
- 
-
-
-
-myPanel.layout.layout(true);
-
+        myPanel.layout.layout(true);
         return myPanel;
     }
 
-//Yes, I made a function for every color. Please dont @ me
-    function onTabClicked(){
+    //Yes, I made a function for every color. Please dont @ me
+    function onTabClicked01(){
         label = 1; 
         runMe(); 
     }
    
-       function onTabClicked2(){
+    function onTabClicked02(){
         label = 2; 
         runMe(); 
     }
-    function onTabClicked3(){
+
+    function onTabClicked03(){
         label = 3; 
         runMe(); 
     }
-    function onTabClicked4(){
+
+    function onTabClicked04(){
         label = 4; 
         runMe(); 
     }
-    function onTabClicked5(){
+
+    function onTabClicked05(){
         label = 5; 
         runMe(); 
     }
-    function onTabClicked6(){
+
+    function onTabClicked06(){
         label = 6; 
         runMe(); 
     }
-    function onTabClicked7(){
+
+    function onTabClicked07(){
         label = 7; 
         runMe(); 
     }
-    function onTabClicked8(){
+
+    function onTabClicked08(){
         label = 8; 
         runMe(); 
     }
-    function onTabClicked9(){
+
+    function onTabClicked09(){
         label = 9; 
         runMe(); 
     }
+
     function onTabClicked10(){
         label = 10; 
         runMe(); 
     }
+
     function onTabClicked11(){
         label = 11; 
         runMe(); 
     }
+
     function onTabClicked12(){
         label = 12; 
         runMe(); 
     }
+
     function onTabClicked13(){
         label = 13; 
         runMe(); 
     }
+
     function onTabClicked14(){
         label = 14; 
         runMe(); 
     }
+
     function onTabClicked15(){
         label = 15; 
         runMe(); 
     }
+
     function onTabClicked16(){
         label = 16; 
         runMe(); 
     }
+
     var myScriptPal = myScript_buildUI(thisObj);
 
     if (myScriptPal != null && myScriptPal instanceof Window){
@@ -136,32 +194,20 @@ myPanel.layout.layout(true);
 }
 
 function runMe(){
-
-
     var layerNum = app.project.activeItem.selectedLayers.length; 
-
     var comp=app.project.activeItem;   
 
+    app.beginUndoGroup("labelME");
 
-    app.beginUndoGroup("Label ME!");
-
-
-    for(b = 0; b < layerNum; b++){
-                
+    for(b = 0; b < layerNum; b++){             
         layer = app.project.activeItem.selectedLayers[b];
         layer.label = label; 
-			
-}
-
-
+    }
 
 	myPanel.parent.show();
     myPanel.update();
 	
     app.endUndoGroup();
-
-
-    
-
 }
 myScript(this);
+
