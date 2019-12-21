530334e22420eba6920a7146c67884f4fa9afea9
Brandon <brandy@scarecrowarts.com>
2019-12-21 17:20:46 -0500

    git

--- labelMe.jsx
+++ labelMe.jsx
@@ -7,34 +7,74 @@
 var scriptPath = File($.fileName).parent.fsName;
 var myPanel;
 var label; 
+var img = [];
+var button = [];
 
 function myScript(thisObj){
     function myScript_buildUI(thisObj){
         myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "labelME", [100, 100, 12, 560], {resizeable:false});
         
-        var img00 = scriptPath + "/labelME/none.png"
-        var img01 = scriptPath + "/labelME/red.png"
-        var img02 = scriptPath + "/labelME/yellow.png"
-        var img03 = scriptPath + "/labelME/aqua.png"
-        var img04 = scriptPath + "/labelME/pink.png"
-        var img05 = scriptPath + "/labelME/lavender.png"
-        var img06 = scriptPath + "/labelME/peach.png"
-        var img07 = scriptPath + "/labelME/seafoam.png"
-        var img08 = scriptPath + "/labelME/blue.png"
-        var img09 = scriptPath + "/labelME/green.png"
-        var img10 = scriptPath + "/labelME/purple.png"
-        var img11 = scriptPath + "/labelME/orange.png"
-        var img12 = scriptPath + "/labelME/brown.png"
-        var img13 = scriptPath + "/labelME/fuchsia.png"
-        var img14 = scriptPath + "/labelME/cyan.png"
-        var img15 = scriptPath + "/labelME/sandstone.png"
-        var img16 = scriptPath + "/labelME/darkgreen.png"
+        img[0] = scriptPath + "/labelME/none.png"
+        img[1] = scriptPath + "/labelME/red.png"
+        img[2] = scriptPath + "/labelME/yellow.png"
+        img[3] = scriptPath + "/labelME/aqua.png"
+        img[4] = scriptPath + "/labelME/pink.png"
+        img[5] = scriptPath + "/labelME/lavender.png"
+        img[6] = scriptPath + "/labelME/peach.png"
+        img[7] = scriptPath + "/labelME/seafoam.png"
+        img[8] = scriptPath + "/labelME/blue.png"
+        img[9] = scriptPath + "/labelME/green.png"
+        img[10] = scriptPath + "/labelME/purple.png"
+        img[11] = scriptPath + "/labelME/orange.png"
+        img[12] = scriptPath + "/labelME/brown.png"
+        img[13] = scriptPath + "/labelME/fuchsia.png"
+        img[14] = scriptPath + "/labelME/cyan.png"
+        img[15] = scriptPath + "/labelME/sandstone.png"
+        img[16] = scriptPath + "/labelME/darkgreen.png"
 
         res = "group\{\orientation:'column',\}";
 
         myPanel.grp = myPanel.add(res);
-        
+        myPanel.margins = 0;
+        myPanel.grp.margins = 0;
+        myPanel.grp.margins.top = 0;
+        myPanel.grp.margins.bottom = 0;
+        myPanel.grp.margins.left = 0;
+        myPanel.grp.margins.right = 0;
+        myPanel.grp.spacing = 0;
+        myPanel.grp.spacing.top = 0;
+        myPanel.grp.spacing.bottom = 0;
+        myPanel.grp.spacing.left = 0;
+        myPanel.grp.spacing.right = 0;
+        myPanel.grp.padding = 0;
+        myPanel.grp.padding.top = 0;
+        myPanel.grp.padding.bottom = 0;
+        myPanel.grp.padding.left = 0;
+        myPanel.grp.padding.right = 0;
+            
         //INITIALIZE BUTTONS
+        for(i = 0; i < 17; i++){
+            button[i] = myPanel.grp.add("iconbutton", undefined, img[i], {style:'toolbutton'}, "button0" + i)
+            
+            button[i].margins = 0;
+            button[i].margins.top = 0;
+            button[i].margins.bottom = 0;
+            button[i].margins.left = 0;
+            button[i].margins.right = 0;
+            button[i].spacing = 0;
+            button[i].spacing.top = 0;
+            button[i].spacing.bottom = 0;
+            button[i].spacing.left = 0;
+            button[i].spacing.right = 0;
+            button[i].padding = 0;
+            button[i].padding.top = 0;
+            button[i].padding.bottom = 0;
+            button[i].padding.left = 0;
+            button[i].padding.right = 0;
+            
+            button[i].backgroundColor = "000000";
+        }        
+        /*
         var button00 = myPanel.grp.add("iconbutton", undefined, img00, {style:'toolbutton'}, "button00")
         var button01 = myPanel.grp.add("iconbutton", undefined, img01, {style:'toolbutton'}, "button01")
         var button02 = myPanel.grp.add("iconbutton", undefined, img02, {style:'toolbutton'}, "button02")
@@ -52,56 +92,45 @@ function myScript(thisObj){
         var button14 = myPanel.grp.add("iconbutton", undefined, img14, {style:'toolbutton'}, "button14")
         var button15 = myPanel.grp.add("iconbutton", undefined, img15, {style:'toolbutton'}, "button15")
         var button16 = myPanel.grp.add("iconbutton", undefined, img16, {style:'toolbutton'}, "button16")
-        
-        myPanel.margins = 0;
-        myPanel.grp.margins = 0;
-        myPanel.grp.margins.top = 0;
-        myPanel.grp.margins.bottom = 0;
-        myPanel.grp.margins.left = 0;
-        myPanel.grp.margins.right = 0;
-        myPanel.grp.spacing = 0;
-        myPanel.grp.spacing.top = 0;
-        myPanel.grp.spacing.bottom = 0;
-        myPanel.grp.spacing.left = 0;
-        myPanel.grp.spacing.right = 0;
+        */
         
         //TOOL TIPS
-        button00.helpTip = "None";
-        button01.helpTip = "Red";
-        button02.helpTip = "Yellow";
-        button03.helpTip = "Aqua";
-        button04.helpTip = "Pink";
-        button05.helpTip = "Lavender";
-        button06.helpTip = "Peach";
-        button07.helpTip = "Sea Foam";
-        button08.helpTip = "Blue";
-        button09.helpTip = "Green";
-        button10.helpTip = "Purple";
-        button11.helpTip = "Orange";
-        button12.helpTip = "Brown";
-        button13.helpTip = "Fuchsia";
-        button14.helpTip = "Cyan";
-        button15.helpTip = "Sandstone";
-        button16.helpTip = "Dark Green";
+        button[0].helpTip = "None";
+        button[1].helpTip = "Red";
+        button[2].helpTip = "Yellow";
+        button[3].helpTip = "Aqua";
+        button[4].helpTip = "Pink";
+        button[5].helpTip = "Lavender";
+        button[6].helpTip = "Peach";
+        button[7].helpTip = "Sea Foam";
+        button[8].helpTip = "Blue";
+        button[9].helpTip = "Green";
+        button[10].helpTip = "Purple";
+        button[11].helpTip = "Orange";
+        button[12].helpTip = "Brown";
+        button[13].helpTip = "Fuchsia";
+        button[14].helpTip = "Cyan";
+        button[15].helpTip = "Sandstone";
+        button[16].helpTip = "Dark Green";
 
         //DO THE THING
-        button00.onClick =  onTabClicked00;
-        button01.onClick =  onTabClicked01;
-        button02.onClick =  onTabClicked02;
-        button03.onClick =  onTabClicked03;       
-        button04.onClick =  onTabClicked04;      
-        button05.onClick =  onTabClicked05;
-        button06.onClick =  onTabClicked06;
-        button07.onClick =  onTabClicked07;
-        button08.onClick =  onTabClicked08;       
-        button09.onClick =  onTabClicked09;      
-        button10.onClick =  onTabClicked10;
-        button11.onClick =  onTabClicked11;
-        button12.onClick =  onTabClicked12;
-        button13.onClick =  onTabClicked13;       
-        button14.onClick =  onTabClicked14;
-        button15.onClick =  onTabClicked15;
-        button16.onClick =  onTabClicked16;
+        button[0].onClick =  onTabClicked00;
+        button[1].onClick =  onTabClicked01;
+        button[2].onClick =  onTabClicked02;
+        button[3].onClick =  onTabClicked03;       
+        button[4].onClick =  onTabClicked04;      
+        button[5].onClick =  onTabClicked05;
+        button[6].onClick =  onTabClicked06;
+        button[7].onClick =  onTabClicked07;
+        button[8].onClick =  onTabClicked08;       
+        button[9].onClick =  onTabClicked09;      
+        button[10].onClick =  onTabClicked10;
+        button[11].onClick =  onTabClicked11;
+        button[12].onClick =  onTabClicked12;
+        button[13].onClick =  onTabClicked13;       
+        button[14].onClick =  onTabClicked14;
+        button[15].onClick =  onTabClicked15;
+        button[16].onClick =  onTabClicked16;
 
         myPanel.layout.layout(true);
         return myPanel;
