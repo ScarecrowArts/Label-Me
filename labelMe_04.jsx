e166a154abaf6e5442cfc1c31c48c6edbb581387
Brandon <brandy@scarecrowarts.com>
2019-12-22 19:09:41 -0500

    fingerprints ABOVE twitch

--- labelMe_04.jsx
+++ labelMe_04.jsx
@@ -14,7 +14,7 @@ var button = [];
 
 function myScript(thisObj){
     function myScript_buildUI(thisObj){
-        myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "labelME", [100, 100, 12, 560], {resizeable:false});
+        myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette", "labelME", [100, 100, 12, 120], {resizeable:false});
         mySaveFilePath = "~/Documents/";
 
         var file = File(mySaveFilePath + "Path" + ".txt");
